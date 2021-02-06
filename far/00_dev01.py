f9357d42f96d7ae67deecfe9563841fe231ea562
smatsushima1 <smatsushima1@gmail.com>
2021-02-06 02:29:13 -0500

    make more changes

--- far/00_dev01.py
+++ far/00_dev01.py
@@ -5,14 +5,93 @@ Spyder Editor
 This is a temporary script file.
 """
 
-
 import os
 from os import path
+import json
+from bs4 import BeautifulSoup as bsp
+import urllib as ul
+import re
 
-if path.exists('json/02_href_reg2s.json'):
-  print(1)
-else:
-  print(2)
+hname1 = 'html/04_part_href.html'
+hname2 = 'html/04_part_href2.html'
+#if path.exists(hname2): open(hname2, 'w').close()
+
+with open(hname2, 'r', encoding = 'utf8') as hn1:
+  contents = hn1.read()
+  soup = bsp(contents, 'html.parser')
+  res = soup.find_all('td', class_ = re.compile('.*part-number'))
+  for i in res:
+    hpart = i.get_text()
+    hpart = hpart.strip()
+    href = i.a['href']
+    href = href.strip()
+    print(str(hpart) + ' - ' + str(href))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# with open(hname2, 'w', encoding = 'utf8') as hn2:
+#   soup = bsp(contents, 'html.parser')
+#   res = soup.find_all('tbody')
+#   for i in res:
+#     hn2.write(str(i.prettify()))
+#     break
+
+
+
+
+
+
+# open the url and save it as an html object
+# resp = ul.request.urlopen(res)
+# html_res = resp.read()
+
+# turn it into html and parse out the content
+# text is always found in the <div id="middlecontent"> area on the webpage
+# soup = bsp(html_res, 'html.parser')
+# reg_txt = soup.find('div', id = 'middlecontent')
+
+# with open(hfile, 'w', encoding = 'utf8') as h:
+#   h.write(reg_txt.prettify())
+#   h.close()
+
+#print(reg_txt.prettify())
+# look into .extrar() for beautiful soup to extract contents in between tags
+# changin the names of tags and attributes
+
+
+
+
+
+# reg
+# part
+# subpart
+# section
+# http_link
+# html
 
 
 
--- far/04_part_href.py
+++ far/04_part_href.py
@@ -10,50 +10,45 @@ import re
 ###############################################################################
 # Functions
 
-# Return html text
-def ret_href(addr):
-  # Open the url and save it as an html object
+# Parse each part for each regulation
+def parts_hrefs(addr):
   srch1 = 'https://www.acquisition.gov'
   srch2 = srch1 + addr
+
+  # Open the url and save it as an html object
   resp = ul.request.urlopen(srch2)
   html_res = resp.read()
   soup = bsp(html_res, 'html.parser')
   
-  # Start writing to html file
-  with open(hname, 'a', encoding = 'utf8') as hf:
-    lbr = '<br>'
-    hf.write(lbr * 5 + addr)
-  
-    # Add before adding everything else
-    # Square brackets = array of objects (list)
-    # Curly brackets = object
-    reg_num = addr.strip('/')
-    jf.write('"' + reg_num + '"' + ': [')
-  
-    # Finding this div applies to FAR, DFARS, and GSAM
-    # It would be 0 if this id wasn't even on the page
-    htxt = soup.find('div', id = 'parts-wrapper')
-    if htxt is not None:
-      htxt = htxt.div
-    else:
-      htxt = soup.tbody
-    hf.write(str(htxt.prettify()))
-    
-  # Then get a list of all the a's, which has our information
-  htxt = htxt.find_all('a')
-  for j in htxt:
-    # The part numbers will always just be the text
-    # Concatenate and complete the href
-    hpart = ret_part(j.get_text())
-    href = srch1 + j.attrs['href']
-    d = {
-         # Figure out how to change to int
-         'part': hpart,
-         'href': href,
-         'html_text': 'N/A'
-        }
-    json.dump(d, jf, indent = 2)
-  
+  # Add before adding everything else
+  # Square brackets = array of objects (list)
+  # Curly brackets = object
+  reg_num = addr.strip('/')
+  jf.write('"' + reg_num + '"' + ': [')
+
+  # Finding this div applies to FAR, DFARS, and GSAM
+  # If there were no results, it would be a null object
+  htxt = soup.find('div', id = 'parts-wrapper')
+  if htxt is not None:
+    htxt = htxt.div
+    # All the a tags have our information within the div tag
+    htxt = htxt.find_all('a')
+    for j in htxt:
+      # The part numbers will always just be the text
+      hpart = ret_part(j.get_text()).strip()
+      href = srch1 + j.attrs['href'].strip()
+      add_to_dict(hpart, href)
+
+  # This only gets run for supplementals
+  # The tbody tag will always have our information
+  # The td tags have long class names, so regex was needed to parse it
+  else:
+    htxt = soup.tbody
+    htxt = soup.find_all('td', class_ = re.compile('.*part-number'))
+    for j in htxt:
+      hpart = ret_part(j.get_text()).strip()
+      href = srch1 + j.a['href'].strip()
+      add_to_dict(hpart, href)
   # Close the bracket  
   jf.write(']')
 
@@ -61,10 +56,20 @@ def ret_href(addr):
 def ret_part(txt):
   txt_sp = txt.split()
   cnt = len(txt_sp)
+  # If its just a number, the list will be 1 object
   if cnt > 1:
     txt = txt_sp[cnt - 1]
   return txt
-  
+
+# Adds values to the dictionary, which dumps json into the file
+def add_to_dict(hpart, href):
+  d = {
+       # Figure out how to change to int
+       'part': hpart,
+       'href': href,
+       'html_text': 'N/A'
+       }
+  json.dump(d, jf, indent = 2, sort_keys = True)
 ###############################################################################
 
 # Load file
@@ -73,10 +78,11 @@ jname = os.path.basename(__file__).split('.')[0]
 jname = 'json/' + jname + '.json'
 if path.exists(jname): open(jname, 'w').close()
 
-# write html for each section just to get an idea of each structure
-hname = os.path.basename(__file__).split('.')[0]
-hname = 'html/' + hname + '.html'
-if path.exists(hname): open(hname, 'w').close()
+# Write html for each section just to get an idea of each structure
+# This would be required at the start, but not once it starts running
+# hname = os.path.basename(__file__).split('.')[0]
+# hname = 'html/' + hname + '.html'
+# if path.exists(hname): open(hname, 'w').close()
 
 # Save all data from hrefs and regs into dictionary to loop through
 jname2 = 'json/02_href_regs.json'
@@ -88,10 +94,10 @@ with open(jname2) as jf2:
 with open(jname, 'w', encoding = 'utf8') as jf:
   jf.write('{')  
   for i in data:
-    reg = i['href']
+    reg = str(i['href'])
     print(reg)
-    ret_href(str(reg))
-
+    parts_hrefs(reg)
+  # Close off final bracket
   jf.write('}')
 
 # Convert all dictionaries to strings, then add commas inbetween each object
@@ -99,6 +105,7 @@ with open(jname, 'w', encoding = 'utf8') as jf:
 with open(jname, 'r') as jf:
   contents = jf.read()
   contents = contents.replace('}{', '},{')
+  contents = contents.replace(']"', '],"')
 with open(jname, 'w', encoding = 'utf8') as jf:
   jf.write(contents)
 
@@ -111,89 +118,3 @@ with open(jname, 'w', encoding = 'utf8') as jf:
 print('Finished pushing data to ' + jname)  
 
   
-
-
-
-
-  #print(html_contents)
-  
-
-  #rt = rt.find_all('a')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#   # Add closing bracket to signify the end
-#   jf.write(']')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# open the url and save it as an html object
-# resp = ul.request.urlopen(res)
-# html_res = resp.read()
-
-# turn it into html and parse out the content
-# text is always found in the <div id="middlecontent"> area on the webpage
-# soup = bsp(html_res, 'html.parser')
-# reg_txt = soup.find('div', id = 'middlecontent')
-
-# with open(hfile, 'w', encoding = 'utf8') as h:
-#   h.write(reg_txt.prettify())
-#   h.close()
-
-#print(reg_txt.prettify())
-# look into .extrar() for beautiful soup to extract contents in between tags
-# changin the names of tags and attributes
-
-
-
-
-
-# reg
-# part
-# subpart
-# section
-# http_link
-# html
-
-
-
-
-
-
-
